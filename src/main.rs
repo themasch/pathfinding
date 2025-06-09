527de1bbb74709bc501dc4cf8bee6094f2258491
Mark Schmale <masch@masch.it>
2025-06-09 15:51:25 +0200

    do the pathfinding
    it does not always find the best path yet, there are at least two problems i see:
    
    The obvious one is that this only uses vertices of the nav mesh as waypoints (except start & end). 
    So two points sitting on two neighboring triangles will always travel over one of the vertices, which is rarely the shortest path.
    Funnelling seems to be the correct way to solve this, TBD. 
    
    But there also seems to be cases where our implementation fails to find the shortest route over these edges.
    I think the check if the current node is one of the edges of our target vertice is to early. 
    If we find one of these three points as our current looked at node we immediately finish, but just because that points belongs
    to the same triangle as our goal does not mean that there is no shorter path using more points to get to another vertix of this triangle
    So we must change that check and tread these points as normal nodes, + handle the special case that our goal is a neighbor of these three.
    
    Other than that: it works, but needs a lot of cleanup!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::{
-    color::palettes::css::WHITE,
+    color::palettes::css::{CRIMSON, WHITE},
+    gizmos,
     input::common_conditions::*,
     math::sampling::UniformMeshSampler,
     pbr::wireframe::{WireframeConfig, WireframePlugin},
@@ -9,7 +10,14 @@ use bevy::{
 use bevy_obj::ObjPlugin;
 use rand::distributions::Distribution;
 use rand::rngs::ThreadRng;
-use std::{collections::HashMap, f32::consts::PI, time::Instant};
+use std::{
+    cell::RefCell,
+    cmp::Ordering,
+    collections::{BinaryHeap, HashMap},
+    f32::{INFINITY, consts::PI},
+    rc::Rc,
+    time::Instant,
+};
 
 fn main() {
     App::new()
@@ -18,8 +26,13 @@ fn main() {
             global: true,
             default_color: WHITE.into(),
         })
+        .add_event::<PathFindingRequest>()
+        .add_event::<PathFound>()
         .add_systems(Startup, setup)
-        .add_systems(Update, generate_new_nav_graph)
+        .add_systems(
+            Update,
+            (generate_new_nav_graph, find_path, spawn_found_path),
+        )
         .add_systems(
             Update,
             regenerate_test_route_parameters.run_if(input_just_pressed(KeyCode::KeyR)),
@@ -92,13 +105,36 @@ type TravelCost = f32;
 
 #[derive(Debug)]
 struct NavNode {
+    pos: Vec3,
     connections: Vec<(NodeIndex, TravelCost)>,
     triangles: Vec<NodeIndex>,
 }
 
 impl NavNode {
-    fn new() -> Self {
+    fn cost_to(&self, other: &NodeIndex) -> Option<TravelCost> {
+        self.connections
+            .iter()
+            .find(|(n, _)| n == other)
+            .map(|(_, c)| *c)
+    }
+
+    fn path_end(pos: Vec3, tri: (NodeIndex, NodeIndex, NodeIndex), graph: &NavGraph) -> Self {
         NavNode {
+            pos: pos,
+            connections: vec![
+                (tri.0, graph.nodes[tri.0].pos.distance(pos)),
+                (tri.1, graph.nodes[tri.1].pos.distance(pos)),
+                (tri.2, graph.nodes[tri.2].pos.distance(pos)),
+            ],
+            triangles: vec![],
+        }
+    }
+}
+
+impl NavNode {
+    fn new(pos: Vec3) -> Self {
+        NavNode {
+            pos,
             connections: Vec::new(),
             triangles: Vec::new(),
         }
@@ -132,9 +168,9 @@ fn generate_new_nav_graph(
             let t_idx = triangles.len();
             triangles.push((v0_idx, v1_idx, v2_idx));
 
-            let dv0v1 = v0.distance_squared(v1);
-            let dv0v2 = v0.distance_squared(v2);
-            let dv1v2 = v1.distance_squared(v2);
+            let dv0v1 = v0.distance(v1);
+            let dv0v2 = v0.distance(v2);
+            let dv1v2 = v1.distance(v2);
 
             nodes[v0_idx].triangles.push(t_idx);
             nodes[v1_idx].triangles.push(t_idx);
@@ -168,7 +204,7 @@ fn register_node(
         .entry((v.x.to_bits(), v.y.to_bits(), v.z.to_bits()))
         .or_insert_with(|| {
             let len = nodes.len();
-            nodes.push(NavNode::new());
+            nodes.push(NavNode::new(v));
             len
         })
 }
@@ -182,6 +218,9 @@ fn regenerate_test_route_parameters(
     nav_mesh: Query<&Mesh3d, (With<NavRoot>, With<NavGraph>)>,
     mut route_start: Query<&mut Transform, (With<Start>, Without<End>)>,
     mut route_end: Query<&mut Transform, (With<End>, Without<Start>)>,
+    mut ev_request_path: EventWriter<PathFindingRequest>,
+    path_gizmos: Query<Entity, With<Gizmo>>,
+    mut commands: Commands,
     meshes: Res<Assets<Mesh>>,
 ) {
     let mut start = route_start.single_mut().unwrap();
@@ -196,6 +235,305 @@ fn regenerate_test_route_parameters(
     let a = sampler.sample(&mut rng);
     let b = sampler.sample(&mut rng);
 
-    start.translation = a;
-    end.translation = b;
+    start.translation = a.clone();
+    end.translation = b.clone();
+
+    ev_request_path.send(PathFindingRequest(a, b));
+
+    for entity in path_gizmos.iter() {
+        commands.entity(entity).despawn();
+    }
+}
+
+#[derive(Event)]
+struct PathFindingRequest(Vec3, Vec3);
+
+#[derive(Event)]
+struct PathFound(Path);
+
+fn find_path(
+    mut ev_path_requests: EventReader<PathFindingRequest>,
+    mut ev_path_found: EventWriter<PathFound>,
+    nav_mesh: Query<&NavGraph, With<NavRoot>>,
+) {
+    let Ok(nav_graph) = nav_mesh.single() else {
+        return;
+    };
+
+    for PathFindingRequest(from, to) in ev_path_requests.read() {
+        let start = Instant::now();
+        let path = astar(&nav_graph, *from, *to);
+        if let Some(path) = path {
+            println!("found path: {}, took {:?}", path.cost(), start.elapsed());
+            ev_path_found.write(PathFound(path));
+        } else {
+            println!("found no path");
+        }
+    }
+}
+
+fn spawn_found_path(
+    mut ev_path_found: EventReader<PathFound>,
+    mut commands: Commands,
+    mut gizmo_assets: ResMut<Assets<GizmoAsset>>,
+) {
+    for PathFound(path) in ev_path_found.read() {
+        let mut path_gizmo = GizmoAsset::default();
+        path_gizmo.linestrip(path.points.iter().map(|n| n.pos), CRIMSON);
+        commands.spawn(Gizmo {
+            handle: gizmo_assets.add(path_gizmo),
+            line_config: GizmoLineConfig {
+                width: 5.0,
+                ..default()
+            },
+            ..default()
+        });
+    }
+}
+
+fn find_triangles(
+    graph: &NavGraph,
+    from: Vec3,
+    to: Vec3,
+) -> (Option<NodeIndex>, Option<NodeIndex>) {
+    let mut start_tri = None;
+    let mut end_tri = None;
+    for (i, tri) in graph.triangles.iter().enumerate() {
+        if start_tri.is_some() && end_tri.is_some() {
+            return (start_tri, end_tri);
+        }
+        if start_tri.is_none()
+            && point_in_triangle(
+                from,
+                graph.nodes[tri.0].pos,
+                graph.nodes[tri.1].pos,
+                graph.nodes[tri.2].pos,
+            )
+        {
+            start_tri = Some(i);
+        }
+
+        if end_tri.is_none()
+            && point_in_triangle(
+                to,
+                graph.nodes[tri.0].pos,
+                graph.nodes[tri.1].pos,
+                graph.nodes[tri.2].pos,
+            )
+        {
+            end_tri = Some(i);
+        }
+    }
+
+    return (start_tri, end_tri);
+}
+
+struct Waypoint {
+    pos: Vec3,
+    node: Option<NodeIndex>,
+}
+
+impl Waypoint {
+    fn for_end(pos: Vec3) -> Self {
+        Self { pos, node: None }
+    }
+
+    fn for_node(pos: Vec3, node: NodeIndex) -> Self {
+        Self {
+            pos,
+            node: Some(node),
+        }
+    }
+}
+
+struct Path {
+    points: Vec<Waypoint>,
+}
+
+impl Path {
+    fn cost(&self) -> f32 {
+        self.points
+            .windows(2)
+            .fold(0.0, |acc, pair| acc + pair[0].pos.distance(pair[1].pos))
+    }
+}
+
+impl From<Vec<Waypoint>> for Path {
+    fn from(points: Vec<Waypoint>) -> Self {
+        Path { points }
+    }
+}
+
+fn astar(graph: &NavGraph, from: Vec3, to: Vec3) -> Option<Path> {
+    let distance = from.distance(to);
+    println!(
+        "need to find a path from {:?} to {:?}, direct distance: {}",
+        from, to, distance
+    );
+    let start = Instant::now();
+
+    let (start_tri, end_tri) = find_triangles(graph, from, to);
+
+    let start_tri_index = start_tri?;
+    let end_tri_index = end_tri?;
+
+    println!(
+        "found triangles that contain our start and end points: {:?} {:?} (took: {:?})",
+        start_tri,
+        end_tri,
+        start.elapsed()
+    );
+
+    if start_tri_index == end_tri_index {
+        println!(
+            "we got a simple path, straight line: {:?} {:?} (distance: {})",
+            from,
+            to,
+            from.distance(to)
+        );
+
+        return Some(
+            vec![
+                Waypoint {
+                    pos: from,
+                    node: None,
+                },
+                Waypoint {
+                    pos: to,
+                    node: None,
+                },
+            ]
+            .into(),
+        );
+    }
+
+    let start_tri = graph.triangles[start_tri_index];
+    let end_tri = graph.triangles[end_tri_index];
+    let goal_nodes = [end_tri.0, end_tri.1, end_tri.2];
+
+    let start_node = NavNode::path_end(from, start_tri, graph);
+    let end_node = NavNode::path_end(to, end_tri, graph);
+
+    let mut pfnodes = HashMap::new();
+    let mut open_set = BinaryHeap::new();
+    for (node_index, cost) in start_node.connections {
+        let node = &graph.nodes[node_index];
+        let g = cost;
+        let f = g + node.pos.distance(end_node.pos);
+        let node = Rc::new(RefCell::new(AStarNode { g, f, node_index }));
+        pfnodes.insert(node_index, node.clone());
+        open_set.push(node.clone());
+    }
+
+    let mut came_from = HashMap::new();
+
+    while !open_set.is_empty() {
+        let current_rc = open_set.pop().unwrap();
+        let current = current_rc.borrow();
+        if goal_nodes.contains(&current.node_index) {
+            //TODO: I think this is not exactly correct, we still need to pick the best of these three, right?
+            let p = reconstruct_path(&came_from, current.node_index);
+            return Some(build_waypoints(p, graph, from, to));
+        }
+
+        let node = &graph.nodes[current.node_index];
+
+        for (n, cost) in &node.connections {
+            let neighbor_index = *n;
+            let neighbor_g = &graph.nodes[neighbor_index];
+            let neighbor_a_rc = pfnodes.entry(neighbor_index).or_insert_with(|| {
+                Rc::new(RefCell::new(AStarNode {
+                    g: INFINITY,
+                    f: INFINITY,
+                    node_index: neighbor_index,
+                }))
+            });
+            let mut neighbor_a = neighbor_a_rc.borrow_mut();
+
+            let tg = current.g + cost;
+
+            if tg < neighbor_a.g {
+                neighbor_a.g = tg;
+                neighbor_a.f = tg + neighbor_g.pos.distance(end_node.pos);
+                came_from.insert(neighbor_index, current.node_index);
+                drop(neighbor_a);
+                open_set.push(neighbor_a_rc.clone());
+            }
+        }
+    }
+
+    None
+}
+
+fn build_waypoints(p: Vec<usize>, graph: &NavGraph, from: Vec3, to: Vec3) -> Path {
+    let nodes = p.iter().map(|nidx| Waypoint {
+        pos: graph.nodes[*nidx].pos,
+        node: Some(*nidx),
+    });
+
+    [Waypoint::for_end(from)]
+        .into_iter()
+        .chain(nodes)
+        .chain([Waypoint::for_end(to)])
+        .collect::<Vec<Waypoint>>()
+        .into()
+}
+
+fn reconstruct_path(came_from: &HashMap<usize, usize>, last_node: usize) -> Vec<usize> {
+    let mut path = Vec::new();
+    let mut current = last_node;
+
+    while let Some(&prev) = came_from.get(&current) {
+        path.push(current);
+        current = prev;
+    }
+
+    path.push(current);
+    path.reverse();
+    path
+}
+
+#[derive(Debug, Copy, Clone)]
+struct AStarNode {
+    // curremtly known cheapest cost from start to here
+    g: f32,
+    // estimates the cheapest complete path start to end
+    f: f32,
+    node_index: NodeIndex,
+}
+
+impl PartialEq for AStarNode {
+    fn eq(&self, other: &Self) -> bool {
+        self.f.total_cmp(&other.f) == Ordering::Equal
+    }
+}
+
+impl Eq for AStarNode {}
+
+impl Ord for AStarNode {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.f.total_cmp(&self.f)
+    }
+}
+
+impl PartialOrd for AStarNode {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        // reveresed because we want a  min-heap
+        Some(other.g.total_cmp(&self.g))
+    }
+}
+
+fn sign(p1: Vec3, p2: Vec3, p3: Vec3) -> f32 {
+    return (p1.x - p3.x) * (p2.z - p3.z) - (p2.x - p3.x) * (p1.z - p3.z);
+}
+
+fn point_in_triangle(pt: Vec3, v1: Vec3, v2: Vec3, v3: Vec3) -> bool {
+    let d1 = sign(pt, v1, v2);
+    let d2 = sign(pt, v2, v3);
+    let d3 = sign(pt, v3, v1);
+
+    let has_neg = (d1 < 0.0) || (d2 < 0.0) || (d3 < 0.0);
+    let has_pos = (d1 > 0.0) || (d2 > 0.0) || (d3 > 0.0);
+
+    return !(has_neg && has_pos);
 }
